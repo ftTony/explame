6ae14429da9f0ea7c0a1f1861a375b519461730f
tony <tony@futunn.com>
2018-12-31 14:13:04 +0800

    floor更新

---
+++ pwa/sw.js
@@ -0,0 +1,14 @@
+var cacheName = 'helloWorld';
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['index.html'])));
+});
+
+self.addEventListener('fetch',function(event){
+    caches.match(event.request).then(function(response){
+        if(response){
+            return response;
+        }
+        return fetch(event.request);
+    });
+});
